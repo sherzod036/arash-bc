60d9e07b87f182f152c94c72e1daa7746eb01dc5
sherzod <sherzod.erkinov036@gmail.com>
2022-02-21 00:20:04 +0500

    Fixed features block animation

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,134 @@
 document.addEventListener('DOMContentLoaded', () => {
+	// TABS
 	const links = document.querySelectorAll('.tabs-link')
 	const contents = document.querySelectorAll('.tabs-content')
+	const officeLinks = document.querySelectorAll('.page-offices__tabs-links a')
+	const officeContents = document.querySelectorAll(
+		'.page-offices__tabs-content'
+	)
 
-	Array.from(links).map((link) => {
-		link.addEventListener('click', function (e) {
-			e.preventDefault()
-			Array.from(links).map((i) => i.classList.remove('tabs-link_active'))
-			Array.from(contents).map((i) => i.classList.remove('tabs-content_active'))
-			const contentId = document.getElementById(link.getAttribute('data-id'))
-			link.classList.add('tabs-link_active')
-			contentId.classList.add('tabs-content_active')
+	function tabs(tLinks, tLinkActive, tContents, tContentActive) {
+		tLinks.forEach((link) => {
+			link.addEventListener('click', function (e) {
+				e.preventDefault()
+				tLinks.forEach((i) => i.classList.remove(tLinkActive))
+				tContents.forEach((i) => i.classList.remove(tContentActive))
+				const contentId = document.getElementById(link.getAttribute('data-id'))
+				link.classList.add(tLinkActive)
+				contentId.classList.add(tContentActive)
+			})
 		})
-	})
-	// wait for document ready
+	}
+
+	tabs(links, 'tabs-link_active', contents, 'tabs-content_active')
+	tabs(
+		officeLinks,
+		'active',
+		officeContents,
+		'page-offices__tabs-content_active'
+	)
+
+	// GSAP + SCROLLMAGIC
+	let simpleTitle = document.querySelector('.advantages__simple-title')
+	let simpleDesc = document.querySelector('.advantages__simple-desc')
+
+	console.log('simpleTitle.textContent()', simpleTitle)
 
 	let controller = new ScrollMagic.Controller()
-	let slidesTotal = 6
-	let transitionWidth = 480 / slidesTotal
 	let horizontalSlide = new TimelineMax()
-		// animate panels
-		.to('.main-advantages', 1, { x: '-' + transitionWidth + '%' })
-		.to('.main-advantages', 1, { x: '-' + transitionWidth * 2 + '%' })
-		.to('.main-advantages', 1, { x: '-' + transitionWidth * 3 + '%' })
-		.to('.main-advantages', 1, { x: '-' + transitionWidth * 4 + '%' })
-		.to('.main-advantages', 1, { x: '-' + transitionWidth * 5 + '%' })
+		.fromTo(
+			'.advantages_v1',
+			{ y: 380 },
+			{
+				y: 0,
+				duration: 1.5,
+				onStart: function () {
+					simpleTitle.innerHTML = 'Офисы в центре города'
+					simpleDesc.innerHTML = 'Офисы в центре города'
+				},
+				onReverseComplete: function () {
+					simpleTitle.innerHTML = 'Офисы в центре города'
+					simpleDesc.innerHTML = 'Офисы в центре города'
+				}
+			}
+		)
+		.fromTo(
+			'.advantages_v2',
+			{ y: 1000 },
+			{
+				y: 0,
+				duration: 1.5,
+				onStart: function () {
+					simpleTitle.innerHTML = 'Коммунальные расходы'
+					simpleDesc.innerHTML = `Арендуя офис в Arash Business Centre вы не будете думать о коммунальных расходах, так как все расходы включены в арендную плату`
+				},
+				onReverseComplete: function () {
+					simpleTitle.innerHTML = 'Коммунальные расходы'
+					simpleDesc.innerHTML = `Арендуя офис в Arash Business Centre вы не будете думать о коммунальных расходах, так как все расходы включены в арендную плату`
+				}
+			}
+		)
+		.fromTo(
+			'.advantages_v3',
+			{ y: 1000 },
+			{
+				y: 0,
+				duration: 1.5,
+				onStart: function () {
+					simpleTitle.innerHTML = 'Парковочное место'
+					simpleDesc.innerHTML =
+						'Удобное, охраняемое парковочное место предусмотренная для каждого офиса'
+				},
+				onReverseComplete: function () {
+					simpleTitle.innerHTML = 'Парковочное место'
+					simpleDesc.innerHTML =
+						'Удобное, охраняемое парковочное место предусмотренная для каждого офиса'
+				}
+			}
+		)
+		.fromTo(
+			'.advantages_v4',
+			{ y: 1000 },
+			{
+				y: 0,
+				duration: 1.5,
+				onStart: function () {
+					simpleTitle.innerHTML = 'Безопасность'
+					simpleDesc.innerHTML =
+						'Ваше офисное помещение всегда находится под круглосуточной охраной, в здании установлены системы видеонаблюдения, пожарные сигнализации и датчики движения'
+				},
+				onReverseComplete: function () {
+					simpleTitle.innerHTML = 'Безопасность'
+					simpleDesc.innerHTML =
+						'Ваше офисное помещение всегда находится под круглосуточной охраной, в здании установлены системы видеонаблюдения, пожарные сигнализации и датчики движения'
+				}
+			}
+		)
+		.fromTo(
+			'.advantages_v5',
+			{ y: 1000 },
+			{
+				y: 0,
+				duration: 1.5,
+				onStart: function () {
+					simpleTitle.innerHTML = 'Чистота'
+					simpleDesc.innerHTML =
+						'В помещениях и общих зонах бизнес-центра всегда поддерживается безупречный порядок и чистота'
+				},
+				onReverseComplete: function () {
+					simpleTitle.innerHTML = 'Чистота'
+					simpleDesc.innerHTML =
+						'В помещениях и общих зонах бизнес-центра всегда поддерживается безупречный порядок и чистота'
+				}
+			}
+		)
 
-	// create scene to pin and link animation
 	new ScrollMagic.Scene({
 		triggerElement: '.mainbig-wrapper',
 		triggerHook: 'onLeave',
-		duration: '100%'
+		duration: '400%'
 	})
 		.setPin('.mainbig-wrapper')
 		.setTween(horizontalSlide)
-		//.addIndicators() // add indicators (requires plugin)
 		.addTo(controller)
 })
